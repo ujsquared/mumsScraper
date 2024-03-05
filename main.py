5f9ede8a592b45ae33e44ade28d68205be42fc69
ujsquared <75967097+ujsquared@users.noreply.github.com>
2024-03-05 22:35:45 +0530

    script is now argparse compatible
    use --h to know more!

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from lxml import etree as et
 from bs4 import BeautifulSoup
 import requests
 import lxml
-stud_id = 323001
-final_id = "b" + str(stud_id)
-for i in range(0,10):
+import json
+import argparse
+parser = argparse.ArgumentParser(description='A script that takes command-line arguments.')
+
+# Define a positional argument
+parser.add_argument('-b','--batchYear', type=int, help='Batch year whose data is being scraped')
+parser.add_argument('-c','--newCookie', type=str, help='Cookie as an input for every data scrape')
+args = parser.parse_args()
+newCookie = args.newCookie
+batchYear = args.batchYear
+def nameGrabber(id):
     cookies = {
-        'PHPSESSID': 'o6siie0s8d50s514t92gd2biu1',
+        'PHPSESSID': newCookie,
     }
 
     headers = {
@@ -33,14 +41,46 @@ for i in range(0,10):
         # 'Cookie': 'PHPSESSID=o6siie0s8d50s514t92gd2biu1',
     }
 
-    data = '------WebKitFormBoundary8C1b1uZrjCPzK8XC\r\nContent-Disposition: form-data; name="PID"\r\n\r\n\r\n------WebKitFormBoundary8C1b1uZrjCPzK8XC\r\nContent-Disposition: form-data; name="POID"\r\n\r\n\r\n------WebKitFormBoundary8C1b1uZrjCPzK8XC\r\nContent-Disposition: form-data; name="SRCHSTR"\r\n\r\n'+str(final_id)+'\r\n------WebKitFormBoundary8C1b1uZrjCPzK8XC--\r\n'
+    data = '------WebKitFormBoundary8C1b1uZrjCPzK8XC\r\nContent-Disposition: form-data; name="PID"\r\n\r\n\r\n------WebKitFormBoundary8C1b1uZrjCPzK8XC\r\nContent-Disposition: form-data; name="POID"\r\n\r\n\r\n------WebKitFormBoundary8C1b1uZrjCPzK8XC\r\nContent-Disposition: form-data; name="SRCHSTR"\r\n\r\n'+id+'\r\n------WebKitFormBoundary8C1b1uZrjCPzK8XC--\r\n'
 
     response = requests.post('https://hib.iiit-bh.ac.in/m-ums-2.0/app.misc/search/stuList.php', cookies=cookies, headers=headers, data=data, verify=False)
 
-    text_response = response.content
-    soup = BeautifulSoup(text_response,'html.parser')
-    print(soup.prettify())
-    print(final_id)
-    stud_id+=1
-    final_id = "b" + str(stud_id)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    target = soup.find('p',{'style':'color:Black;text-decoration: none; white-space: normal;line-height: 1.0em;'})
+    if target == None:
+        return None
+    else:
+        return target.text
+
+choice  = 1
+noDataFlag = False
+count = 0
+dict_stud = [["sno","id","name"]]
+
+for i in range(1,6):
+    choice  = 1
+    noDataFlag = False
+    lastFive = int(batchYear)*1000 + 1
+    while(choice == 1):
+       while(noDataFlag == False):
+             idFinal = 'b'+str(i)+str(lastFive)
+             print(idFinal)
+             nameMacha = nameGrabber(idFinal)    
+             if(nameMacha == None):
+                print("wtf error thrown catch kro")
+                choice  = int(input(" +1 id(1) or +1 branch(0)? or  23 to move out of this  matrix"))
+                if choice == 1:
+                    lastFive+=1
+                else:
+                    noDataFlag = True
+             else:
+               dict_stud.append([count,idFinal,nameMacha])
+               lastFive +=1
+               count +=1
+               print(nameMacha)
+fileName = "Batch of '"+str(lastFive)[0:2]
+print(fileName)
+with open(fileName,"w") as outfile:
+    json.dump(dict_stud, outfile)
+print("The total student in this batch are - " + str(count))
 
